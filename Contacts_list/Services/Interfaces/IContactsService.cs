d0ef18037d5b60bd948a0cf75a758520afcf90de
Rafael-PC <rafaelreinertd@hotmail.com>
2024-05-16 14:39:07 -0300

    feat: ContactsSevice interface

---
+++ Contacts_list/Services/Interfaces/IContactsService.cs
@@ -0,0 +1,12 @@
+﻿using Contacts_list.Models;
+
+namespace Contacts_list.Services.Interfaces
+{
+    public interface IContactsService
+    {
+        Task CreateContact(ContactModel contact);
+        Task<ContactModel?> GetContactById(int id);
+        Task UpdateContact(ContactModel contact);
+        Task DeleteContactById(int id);
+    }
+}
