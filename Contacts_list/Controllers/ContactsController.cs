3b82e2d740ec0e94ca9d6f0f91531ed85343514f
Rafael-PC <rafaelreinertd@hotmail.com>
2024-05-16 15:17:43 -0300

    feat: ContactsController

---
+++ Contacts_list/Controllers/ContactsController.cs
@@ -0,0 +1,63 @@
+﻿using Contacts_list.Models;
+using Contacts_list.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts_list.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactsService _contactsService;
+
+        public ContactsController(IContactsService contactsService)
+        {
+            _contactsService = contactsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ContactModel>>> GetContacts()
+        {
+            return Ok(await _contactsService.GetContacts());
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<ContactModel>> GetContactById(int id)
+        {
+            var contact = _contactsService.GetContactById(id);
+
+            if (contact == null) return NotFound("Contact not found!");
+
+            return Ok(contact);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(ContactModel contact)
+        {
+            await _contactsService.CreateContact(contact);
+
+            return Ok("Contact created!");
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateContact(int id, ContactModel contact)
+        {
+            var updatedContact = await _contactsService.UpdateContact(id, contact);
+
+            if (updatedContact == null) return NotFound("Contact not found!");
+
+            return Ok("Contact updated!");
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            var deletedContact = await _contactsService.DeleteContactById(id);
+            
+            if (deletedContact == null) return NotFound("Contact not found!");
+
+            return Ok("Contact Deleted!");
+        }
+    }
+}
