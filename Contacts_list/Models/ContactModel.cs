1fe1d08dbaf2b84097c21d661777b17448b7a138
Rafael-PC <rafaelreinertd@hotmail.com>
2024-05-16 14:13:06 -0300

    feat: ContactModel

---
+++ Contacts_list/Models/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Contacts_list.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; } = String.Empty;
+        public string BusinessPhone { get; set; } = String.Empty;
+        public string PrivatePhone { get; set; } = String.Empty;
+        public List<string> Email { get; set; } = new List<string>();
+    }
+}
