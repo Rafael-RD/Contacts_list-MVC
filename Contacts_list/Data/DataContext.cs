6ea566c0164904197e15e5272b5c264119ba2d39
Rafael-PC <rafaelreinertd@hotmail.com>
2024-05-16 14:17:43 -0300

    feat: DataContext

---
+++ Contacts_list/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Contacts_list.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts_list.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+    }
+}
